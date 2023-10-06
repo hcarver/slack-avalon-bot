87d3f872ad9826ccf093087b80a943f0fe8aaa2a
Amyas Merivale <merivale@gmail.com>
2023-10-06 14:03:45 +0200

    Exit process if token not found

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ try {
   token = pathToken || fs.readFileSync("token.txt", "utf8").trim();
 } catch (error) {
   console.log("Your API token should be placed in a 'token.txt' file, which is missing.");
-  return;
+  process.exit(1);
 }
 
 const app = express();
