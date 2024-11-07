7235eb94f3262dbf4b2baddf044cba308a130fec
Hywel Carver <>
2024-11-07 14:15:32 +0000

    Remove unused code

--- src/message-helpers.ts
+++ src/message-helpers.ts
@@ -2,13 +2,6 @@
 const _ = require("lodash");
 
 class MessageHelpers {
-  // Public: Checks whether the message text contains an @-mention for the
-  // given user.
-  static containsUserMention(messageText, userId) {
-    let userTag = `<@${userId}>`;
-    return messageText && messageText.trim().startsWith(userTag);
-  }
-
   static formatAtUser(user_id) {
     // Accept either an id string, or an object like {id: some_string}.
     // This isn't beautiful.
@@ -20,12 +13,6 @@ class MessageHelpers {
     return userArray
       .map((user) => MessageHelpers.formatAtUser(user))
       .join(", ");
-  }
-
-  static pts(n, space, dollar) {
-    space = space || 3;
-    dollar = dollar || "";
-    return `${n < 0 ? "-" : "+"}${_.padStart(dollar + Math.abs(n), space - 1)}`;
   }
 
   static get CLOCK() {
@@ -51,10 +38,6 @@ class MessageHelpers {
     }
     let CLOCK = MessageHelpers.CLOCK;
     return ` in ${CLOCK[t % CLOCK.length]}${t}s`;
-  }
-
-  static fix(s, n) {
-    return _.padEnd(_.truncate(s, { length: n - 1, omission: "…" }), n);
   }
 }
 
